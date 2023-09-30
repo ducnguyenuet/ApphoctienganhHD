a6cb4b6f098fa54ed43fb2f789b0f8dd9c5780ee
Le Viet Hoang <22028006@vnu.edu.vn>
2023-09-30 23:26:10 +0700

    Update Dictionary.java
    them ham insert, thuc hien them 1 word vao dictionary

--- Main/src/main/java/Dictionary.java
+++ Main/src/main/java/Dictionary.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
 
 public class Dictionary {
-    private ArrayList<Word> List;
+    private ArrayList<Word> List = new ArrayList<>();
     public ArrayList<Word> getList()
     {
         return this.List;
+    }
+
+    /**
+     * Inserts a word into the dictionary.
+     *
+     * @param word the word to be inserted
+     */
+
+    public void insert(Word word) {
+        List.add(word);
     }
 }
