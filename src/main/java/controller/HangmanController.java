136ee54269e2022c4312ff7394f6b5dc5330ee09
Le Viet Hoang <22028006@vnu.edu.vn>
2023-12-05 01:01:22 +0700

    them am thanh cho Hangman

--- src/main/java/controller/HangmanController.java
+++ src/main/java/controller/HangmanController.java
@@ -8,6 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -27,6 +29,16 @@ public class HangmanController {
     private String keyText ="";
     private String enter= "";
 
+    Media mp1 = new Media(new File("src/main/resources/sound/correctHang.mp3").toURI().toString());
+    Media mp2 = new Media(new File("src/main/resources/sound/incorrectHang.mp3").toURI().toString());
+    Media mp3 = new Media(new File("src/main/resources/sound/winHang.mp3").toURI().toString());
+    Media mp4 = new Media(new File("src/main/resources/sound/loseHang.mp3").toURI().toString());
+    Media themeSongH = new Media(new File("src/main/resources/sound/themeSongHang.mp3").toURI().toString());
+    MediaPlayer mediaPlayer1 = new MediaPlayer(mp1);
+    MediaPlayer mediaPlayer2 = new MediaPlayer(mp2);
+    MediaPlayer mediaPlayer3 = new MediaPlayer(mp3);
+    MediaPlayer mediaPlayer4 = new MediaPlayer(mp4);
+    MediaPlayer mediaPlayer = new MediaPlayer(themeSongH);
 
     public WordOfDB getKeyWord() {
         return keyWord;
@@ -36,8 +48,14 @@ public class HangmanController {
         this.keyWord = keyWord;
     }
 
+    public void playBackgroundMusic() {
+        //mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
+
     public void setKey()
     {
+        playBackgroundMusic();
         Database db = new Database();
         ArrayList<WordOfDB> List = db.getAllWord();
         int k = List.size();
@@ -72,7 +90,9 @@ public class HangmanController {
     }
 
 
+
     public void guest(KeyEvent keyEvent) {
+
         if (keyEvent.getCode() == KeyCode.ENTER) {
             String ip = input.getText();
             input.clear();
@@ -84,6 +104,8 @@ public class HangmanController {
             } else{
                 if(keyWord.getWord_target().contains(ip))
                 {
+                    mediaPlayer1.stop();
+                    mediaPlayer1.play();
                     int k = keyWord.getWord_target().length();
                     StringBuilder stringBuilder = new StringBuilder(keyText);
                     for(int i=0;i<k;i++)
@@ -96,26 +118,39 @@ public class HangmanController {
                         }
                     }
                     if(!key.getText().contains("_"))
-                    {
+                    {   mediaPlayer.stop();
+                        mediaPlayer3.stop();
+                        mediaPlayer3.play();
                         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                         alert.setTitle("You win!");
                         alert.setHeaderText(keyWord.getInfo());
                         alert.setContentText("Good job ^^");
+                        alert.setOnCloseRequest(event -> {
+                            setKey();
+                        });
                         alert.show();
-                        setKey();
+
                     }
                 } else {
+                    mediaPlayer2.stop();
+                    mediaPlayer2.play();
                     turnLeft--;
                     ytl.setText(String.valueOf(turnLeft));
                     setImage(turnLeft);
                     if(turnLeft == 0)
                     {
+                        mediaPlayer.stop();
+                        mediaPlayer4.stop();
+                        mediaPlayer4.play();
                         Alert alert = new Alert(Alert.AlertType.ERROR);
                         alert.setTitle("Your lose");
                         alert.setHeaderText(keyWord.getInfo());
                         alert.setContentText("try your best next time!");
+                        alert.setOnCloseRequest(event -> {
+                            setKey();
+                        });
                         alert.show();
-                        setKey();
+
                     }
                 }
                 enter = enter + ip +", ";
